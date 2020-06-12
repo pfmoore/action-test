dbeb2c8027207b44bbe499576ca2ac0f8e2993eb
Paul Moore <p.f.moore@gmail.com>
2020-06-12 18:02:25 +0100

    Make this a pure test repo

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-git clone https://github.com/vim/vim
-cd vim\src
-nmake /f make_mvc.mak CPUNR=i686 WINVER=0x0500 LUA=.\lua LUA_VER=53
-nmake /f make_mvc.mak GUI=yes DIRECTX=yes CPUNR=i686 WINVER=0x0500 LUA=.\lua LUA_VER=53
